aadb9e167d6c649683ad5d765992989a6d980c8d
Mikhail T. <m.tridchikov@gmail.com>
2024-06-18 17:13:44 +0300

    Connected ngrok local http

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 const { Telegraf } = require('telegraf');
+const express = require('express');
 
 const bot = new Telegraf('6373456937:AAGYZz2FlvzjJQI6hpf26hUQYJkKX7ByHeA');
 
@@ -7,10 +8,23 @@ bot.start((ctx) => {
   ctx.reply('Welcome to the MiniApp! Click the link below to start using the app:', {
     reply_markup: {
       inline_keyboard: [[
-        { text: "Open MiniApp", url: "http://192.168.1.16:3000" }
+        { text: "Open MiniApp", url: "https://192.168.0.188:3000" }
       ]]
     }
   });
 });
 
+// Запуск бота
+bot.launch()
+  .then(() => console.log('Bot is running...'))
+  .catch(err => console.error('Failed to launch bot', err));
+
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
+
 module.exports = bot;
+
+
+
+// https://192.168.0.188:3000
+// 6373456937:AAGYZz2FlvzjJQI6hpf26hUQYJkKX7ByHeA
--- server.js
+++ server.js
@@ -1,37 +1,38 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const bot = require('./bot');
 
 const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
 const server = http.createServer(app);
 const io = socketIo(server);
 
-// Настройка Socket.IO
+const PORT = process.env.PORT || 3001;
+
+app.use(bot.webhookCallback('/webhook-path'));
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  // Введите ваш URL вручную после запуска ngrok
+  const ngrokUrl = 'https://b6fb-2a03-f80-4416-1f1a-00-1.ngrok-free.app'; // Замените на ваш URL
+  bot.telegram.setWebhook(`${ngrokUrl}/webhook-path`)
+    .then(() => {
+      console.log(`Webhook set to ${ngrokUrl}/webhook-path`);
+      // bot.launch() вызывается в bot.js, его не нужно вызывать здесь
+    })
+    .catch(err => console.error('Failed to set webhook or launch bot', err));
+});
+
+// Обработка соединений через Socket.io
 io.on('connection', (socket) => {
   console.log('New client connected');
-  socket.emit('initialData', { coinRate: 100 });
 
   socket.on('disconnect', () => {
     console.log('Client disconnected');
   });
 });
-
-// Использование ngrok для публичного URL (замените на ваш URL)
-const NGROK_URL = 'https://f109-91-132-95-56.ngrok-free.app';
-
-// Настройка webhook для бота
-bot.telegram.setWebhook(`${NGROK_URL}/webhook-path`);
-
-// Обработка обновлений от Telegram
-app.use(bot.webhookCallback('/webhook-path'));
-
-// Запуск сервера
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-  bot.launch().then(() => {
-    console.log('Bot is running...');
-  });
-});
-
